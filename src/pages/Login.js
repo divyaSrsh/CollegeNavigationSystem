ab94aad26666ffbae13f289b5c6ca51ade53b410
divyaSrsh <medivya@cet.ac.in>
2022-07-14 20:00:35 +0530

    first commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,31 +1,40 @@
 import React from 'react'
-import {Form,Button} from "react-bootstrap";
+import {Col, Container, Row, Form,Button} from "react-bootstrap";
+
 
 function Login() {
   return (
-    <div>
-     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Container>
+      <Row>
+        <Col lg={4} md={6} sm={12}>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-    </div>
-  )
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+
+        <Col lg={4} md={6} sm={12}>
+
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Login;
