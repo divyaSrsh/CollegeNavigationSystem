908d11bee9eb31339bd640b673fd02716300c743
HariSK20 <56635847+HariSK20@users.noreply.github.com>
2022-09-19 08:35:33 +0530

    Added GetPointsData and Loader for PSQL connectivity

---
+++ src/db_connections/GetPointsData.js
@@ -0,0 +1,69 @@
+require('dotenv').config({ path: '../../cred.env' })
+// console.log(__dirname);
+const { pool } = require("./pg_db");
+
+async function getAllPoints() {
+  try {
+    const res = await pool.query("SELECT * FROM cse_map");
+    console.log(res.rows);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// retrieveData()
+
+async function getPointData(x) {
+	try{
+		const res = await pool.query(`SELECT * FROM cse_map WHERE id='${x}'`);
+		return res;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+// (async () => {
+// 	console.log(await getPointData('CS_304'));
+// })();
+
+// module.exports = {getAllPoints, getPointData}
+// async function get_point_details(x = 'CS_109'){
+	
+// 	const user = process.env.NEO4J_USERNAME;
+
+// 	const uri = process.env.NEO4J_URI;
+// 	const password = process.env.NEO4J_PASSWORD;
+
+   
+// 	let result = [];
+
+// 	try {
+// 	} catch (error) {
+// 	  console.error('Something went wrong: ', error)
+// 	} finally {
+// 	  await session.close()
+// 	}
+   
+// 	// Don't forget to close the driver connection when you're finished with it
+// 	await driver.close()
+// 	// return(result)
+// 	return(result)
+
+// };
+// //    (async() => {
+// // 	console.log(await result)
+// // 	return result; 
+// //    });
+
+// // export default get_paths;
+(async() => {
+	let res = await getPointData('CS_302');
+	let res2 = JSON.parse(JSON.stringify(await res));
+	let res4 = {};
+	// let res3 = await res2.command;
+	res4.command = await res2.command;
+	res4.rowCount = await res2.rowCount;
+	res4.oid = await res2.oid;
+	res4.rows = await res2.rows;
+	console.log(await res4);
+})();
---
+++ src/db_connections/pg_db.js
@@ -0,0 +1,14 @@
+require('dotenv').config({ path: './cred.env' });
+const { Pool } = require("pg");
+
+
+console.log(process.env.PSQL_USER)
+const pool = new Pool({
+  user: process.env.PSQL_USER,
+  database: process.env.PSQL_DATABASE,
+  password: process.env.PSQL_PASSWORD,
+  port: process.env.PSQL_PORT,
+  host: process.env.PSQL_HOST,
+});
+
+module.exports = { pool };
