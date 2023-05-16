8faa145b7da088462b8f3429b5e3aed94e944391
HariSK20 <56635847+HariSK20@users.noreply.github.com>
2023-05-16 18:14:53 +0530

    Added path_split.js to split path into floors

---
+++ src/pages/path_split.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const data = {
+	'src': 'CS_101',
+	'dest': 'CS_302',
+	'dept': 'cse'
+};
+  
+axios.post('http://127.0.0.1:5000/shortestpath', data)
+.then(response => {
+	// Handle the response from the API
+	// console.log(response.data);
+	let length = 3
+	let data = response.data.path;
+	let path = Array.from({ length }, () => []);
+	let ids = [];
+	// console.log(data);
+	let floor = Number(data[0].z);
+	let temp = [], temp2 = []
+	for (let i = 0; i < data.length; i++)
+	{
+		if(Number(data[i].z) != floor)
+		{
+			path[floor] = temp;
+			temp = [[data[i].fx, data[i].fy]]
+			ids[floor] = temp2 
+			temp2 = [data[i].id]
+			floor = Number(data[i].z)
+		}
+		else 
+		{
+			temp.push([data[i].fx, data[i].fy])
+			temp2.push(data[i].id);
+		}
+	}
+	if(temp.length > 0)
+	{
+		path.push(temp)
+		ids.push(temp2)
+	}
+	console.log(ids);
+	console.log(path);
+})
+.catch(error => {
+	// Handle any errors that occurred during the request
+	console.error(error);
+});
